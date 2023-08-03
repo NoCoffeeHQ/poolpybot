197bd3c3e581b233175f9d7bec1202394a1b25e5
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-08-04 01:24:44 +0200

    fix a small issue caused by Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'authentication/sign_up#new', as: :new_sign_up
   post 'sign_up', to: 'authentication/sign_up#create', as: :sign_up
 
-  get sign_in: 'authentication/sign_in#new', as: :new_sign_in
-  post sign_in: 'authentication/sign_in#create', as: :sign_in
-  post sign_out: 'authentication/sign_in#destroy', as: :sign_out
+  get 'sign_in', to: 'authentication/sign_in#new', as: :new_sign_in
+  post 'sign_in', to: 'authentication/sign_in#create', as: :sign_in
+  post 'sign_out', to: 'authentication/sign_in#destroy', as: :sign_out
 
   # Routes when logged in
   get 'dashboard', to: 'dashboard#index', as: :dashboard
