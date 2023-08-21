9087ea9e64fc7bebe532e233b1c9e5c1d2ead165
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-08-21 13:05:52 +0200

    use a different from address when sending emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV.fetch('NO_REPLY_EMAIL_ADDRESS', 'from@example.com')
   layout 'mailer'
 end
