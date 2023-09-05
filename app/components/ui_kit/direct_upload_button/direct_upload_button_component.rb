057889e0e04b3bd83463e9a43bdc7a18ca521e5d
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-09-05 11:05:42 +0200

    let users upload invoices from anywhere in the application

--- app/components/ui_kit/direct_upload_button/direct_upload_button_component.rb
+++ app/components/ui_kit/direct_upload_button/direct_upload_button_component.rb
@@ -3,18 +3,27 @@
 module UIKit
   module DirectUploadButton
     class DirectUploadButtonComponent < ViewComponent::Base
-      attr_reader :label, :url, :accept, :multiple
+      attr_reader :label, :url, :accept, :multiple, :size
 
-      def initialize(label:, url:, accept: nil, multiple: false)
+      def initialize(label:, url:, accept: nil, size: :md, multiple: false)
         super
         @label = label
         @url = url
         @accept = accept
         @multiple = multiple
+        @size = size
       end
 
       def uploading_label
         t('.uploading_label')
+      end
+
+      def size_classes
+        case size
+        when :xs then 'py-1 px-2 text-xs'
+        when :sm then 'py-1 px-2 text-sm'
+        else 'py-2 px-3 text-sm'
+        end
       end
     end
   end
