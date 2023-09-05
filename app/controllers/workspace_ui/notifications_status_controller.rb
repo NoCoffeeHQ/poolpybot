75c0f572ef0059439cbf29aca6feb7a78aff2b9e
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-09-05 10:32:26 +0200

    translate the notifications in French

--- app/controllers/workspace_ui/notifications_status_controller.rb
+++ app/controllers/workspace_ui/notifications_status_controller.rb
@@ -4,7 +4,6 @@ module WorkspaceUI
   class NotificationsStatusController < BaseController
 
     def show
-      # unread = current_user.notifications_read_at ? current_company.notifications.where(Notification[:created_at].gt(current_user.notifications_read_at)).exists? : true
       respond_to do |format|
         format.json { render json: { unread: current_company.notifications.read?(current_user) } }
       end
